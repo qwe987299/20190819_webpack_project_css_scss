2211c5d195e5b1f445bc499625f721ab9939b7aa
Feng, Cheng-Chi <qwe987299@gmail.com>
2019-08-19 23:27:18 +0800

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,8 @@
+/* Webpack 進入點 */
+
+import script from './script';
+import './style.css';
+import './style.scss';
+
+script.foo1();
+script.foo2();
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+/* Webpack 設定檔 */
+
+const path = require('path');
+
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader', // creates style nodes from JS strings
+          'css-loader', // translates CSS into CommonJS
+          'sass-loader', // compiles Sass to CSS, using Node Sass by default
+        ],
+      },
+    ],
+  },
+};
